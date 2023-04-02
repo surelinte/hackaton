3d8fe8f0c22e82e273308fef1220e504ae6c5184
Lana <bolotova.svetl@gmail.com>
2023-04-02 19:38:09 +0300

    триггер

--- Hackaton/Assets/Resources/Scripts/triggerLogic.cs
+++ Hackaton/Assets/Resources/Scripts/triggerLogic.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class triggerLogic : MonoBehaviour
 {
-    public Animator Animator;
+ public Animator anim;
 
     void Start()
     {
-      //  Animator.SetBool("shot", false);
+        anim.ResetTrigger("shot");
     }
 
     public void Shot()
     {
-     //   Animator.SetActive(true);
-        Animator.Play("Trigger_on", 0, 1);
-        // Animator.SetBool("shot", false);
-        // Animator.SetBool("shot", true);
+       // Animator anim = this.GetComponent<Animator>();
+        //  anim.Play("Trigger", 0, 1);
+        // anim.Play("Trigger_on");
+        anim.SetTrigger("shot");
         AudioSource player = (AudioSource)FindObjectOfType(typeof(AudioSource));
         AudioClip click = Resources.Load<AudioClip>("Audio/click");
         player.PlayOneShot(click);
